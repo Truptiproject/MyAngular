63ca803fd4a825929857a44a8f53c5d2cd280cb0
Truptiproject <trthakare96@gmail.com>
2020-09-14 19:34:37 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+
+
+const routes: Routes = [
+  {path:"sign-in",component:SignInComponent},
+  {path:"sign-up",component:SignUpComponent},
+  {path:"homepage",component:HomepageComponent},
+  {path:"gallery",component:GalleryComponent}
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationBarComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomepageComponent,
+    GalleryComponent,
+    AdminLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
